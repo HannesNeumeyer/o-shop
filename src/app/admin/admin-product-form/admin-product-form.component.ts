b8c2aacbfb59fd0b5ec0d1236b93d6ae3b0630eb
HannesNeumeyer <hannesneu@mail.com>
2018-02-23 16:37:35 +0100

    Edit/Fill Form via productId from database and url param

--- src/app/admin/admin-product-form/admin-product-form.component.ts
+++ src/app/admin/admin-product-form/admin-product-form.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../category.service';
 import { ProductService } from '../../product.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/take';
 
 @Component({
   selector: 'app-admin-product-form',
@@ -10,11 +11,18 @@ import { Router } from '@angular/router';
 })
 export class AdminProductFormComponent implements OnInit {
   category$;
+  id;
+  product = {};
 
-  constructor(private categoryService: CategoryService, private productService: ProductService, private router: Router) { }
+  constructor(private categoryService: CategoryService, private productService: ProductService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.category$ = this.categoryService.getAll();
+
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (this.id){
+      this.productService.get(this.id).take(1).subscribe(p => this.product = p);
+    } 
   }
 
   save(product){
