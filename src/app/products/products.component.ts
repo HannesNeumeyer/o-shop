d62ecc660ccf098721e8ce70ac147ebd302f4bb9
HannesNeumeyer <hannesneu@mail.com>
2018-02-23 18:13:17 +0100

    Filter products on category

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,21 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../product.service';
 import { CategoryService } from '../category.service';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
-  products$;
+export class ProductsComponent implements OnInit, OnDestroy {
+  products = [];
+  filteredProducts = [];
+  subscription: Subscription;
   categories$;
+  category;
 
-  constructor(private productService: ProductService, private categoryService: CategoryService) { }
+  constructor(private productService: ProductService, private categoryService: CategoryService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.products$ = this.productService.getAll();
+    this.subscription = this.productService.getAll().subscribe(p => {
+      this.filteredProducts = this.products = p;
+      
+      this.route.queryParamMap.subscribe(params => {
+        this.category = params.get('category');
+        this.applyFilter();
+      })
+    })
     this.categories$ = this.categoryService.getAll();
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
+
+  private applyFilter(){
+    this.filteredProducts = (this.category) 
+    ? this.products.filter(p => p.categories === this.category)
+    : this.products;
+    console.log(this.category)
   }
 
 }
